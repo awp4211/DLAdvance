23d84064ddcdf2adf7d900b716f45596f2ebbb24
awp4211 <1097028825@qq.com>
2017-06-18 16:06:15 +0800

    SRCNN

--- srcnn/model.py
+++ srcnn/model.py
@@ -1,10 +1,14 @@
 
 import time
 import os
-import matplotlib.pyplot as plt
-import numpy as np
 import tensorflow as tf
 
+from utils import (
+    read_data,
+    input_setup,
+    imsave,
+    merge
+)
 
 class SRCNN(object):
 
@@ -48,13 +52,98 @@ class SRCNN(object):
         self.saver = tf.train.Saver()
 
     def model(self):
+        print("image shape = {0}".format(self.images.get_shape()))#(?,33,33,1)
         conv1 = tf.nn.relu(tf.nn.conv2d(self.images, self.weights['w1'],
                                         strides=[1, 1, 1, 1], padding='VALID')
-                           +self.biases['b1'])
+                           + self.biases['b1'])
+        print("conv1 shape = {0}".format(conv1.get_shape()))#(?,25,25,64)
         conv2 = tf.nn.relu(tf.nn.conv2d(conv1, self.weights['w2'],
                                         strides=[1, 1, 1, 1], padding='VALID')
-                           +self.biases['b2'])
+                           + self.biases['b2'])
+        print("conv2 shape = {0}".format(conv2.get_shape()))#(?,25,25,32)
         conv3 = tf.nn.relu(tf.nn.conv2d(conv2, self.weights['w3'],
                                         strides=[1, 1, 1, 1], padding='VALID')
-                           +self.biases['b3'])
+                           + self.biases['b3'])
+        print("conv3 shape = {0}".format(conv3.get_shape()))#(?,21,21,1)
         return conv3
+
+    def train(self, config):
+        nx = 0
+        ny = 0
+        if config.is_train:
+            input_setup(self.sess, config)
+        else:
+            nx, ny = input_setup(self.sess, config)
+
+        if config.is_train:
+            data_dir = os.path.join("./{}".format(config.checkpoint_dir), "train.h5")
+        else:
+            data_dir = os.path.join("./{}".format(config.checkpoint_dir), "test.h5")
+
+        train_data, train_label = read_data(data_dir)
+        self.train_op = tf.train.GradientDescentOptimizer(config.learning_rate).minimize(self.loss)
+
+        tf.global_variables_initializer().run()
+
+        counter = 0
+        start_time = time.time()
+
+        if self.load(self.checkpoint_dir):
+            print(" [*] Load Success")
+        else:
+            print(" [!] Load failed")
+
+        if config.is_train:
+            print("Training .......")
+            for epoch in xrange(config.epoch):
+                batch_idxs = len(train_data) // config.batch_size
+                for idx in xrange(0, batch_idxs):
+                    batch_images = train_data[idx*config.batch_size: (idx+1)*config.batch_size]
+                    batch_labels = train_label[idx*config.batch_size: (idx+1)*config.batch_size]
+
+                    counter += 1
+                    _, err = self.sess.run([self.train_op, self.loss], feed_dict={
+                        self.images: batch_images,
+                        self.labels: batch_labels
+                    })
+
+                    if counter % 10 == 0:
+                        print("Epoch: [%2d], step: [%2d], time: [%4.4f], loss: [%.8f]"
+                              % ((epoch+1), counter, time.time()-start_time, err))
+                    if counter % 500 == 0:
+                        self.save(config.checkpoint_dir, counter)
+        else:
+            print("Testing ......")
+            result = self.pred.eval({self.images: train_data, self.labels: train_label})
+            result = merge(result, [nx, ny])
+            result = result.squeeze()
+            image_path = os.path.join(os.getcwd(), config.sample_dir)
+            image_path = os.path.join(image_path, "test_image.png")
+            result *= 255.
+            imsave(result, image_path)
+
+    def save(self, checkpoint_dir, step):
+        model_name = "SRCNN.model"
+        model_dir = "%s_%s" % ("srcnn", self.label_size)
+        checkpoint_dir = os.path.join(checkpoint_dir, model_dir)
+
+        if not os.path.exists(checkpoint_dir):
+            os.makedirs(checkpoint_dir)
+
+        self.saver.save(self.sess,
+                        os.path.join(checkpoint_dir, model_name),
+                        global_step=step)
+
+    def load(self, checkpoint_dir):
+        print(" [*] Reading checkpoints ...")
+        model_dir = "%s_%s" % ("srcnn", self.label_size)
+        checkpoint_dir = os.path.join(checkpoint_dir, model_dir)
+
+        ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
+
+        if ckpt and ckpt.model_checkpoint_path:
+            ckpt_name = os.path.basename(ckpt.model_checkpoint_path)
+            self.saver.restore(self.sess, os.path.join(checkpoint_dir, ckpt_name))
+            return True
+        else:
+            return False
