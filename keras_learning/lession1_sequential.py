f66153492a860a52c2748aba9b2eb12ecb73def0
awp4211 <1097028825@qq.com>
2017-07-04 11:05:47 +0800

    keras lession1

--- keras_learning/lession1_sequential.py
+++ keras_learning/lession1_sequential.py
@@ -101,19 +101,80 @@ def vgg_demo():
     score = model.evaluate(x_test, y_test, batch_size=32)
 
 
-def lstm_demo():
+def stack_lstm_demo():
     """
     LSTM in keras
     :return:
     """
     import keras
+    import numpy as np
+
     from keras.models import Sequential
     from keras.layers import Dense, Dropout
     from keras.layers import Embedding
     from keras.layers import LSTM
 
+    data_dim = 16
+    timesteps = 8
+    num_classes = 10
+
+    # expected input data shape : (batch_size, timesteps, data_dim)
     model = Sequential()
-    model.add(Embedding())
+    model.add(LSTM(32, return_sequences=True,
+                   input_shape=(timesteps, data_dim)))
+    model.add(LSTM(32, return_sequences=True))
+    model.add(LSTM(32))
+    model.add(Dense(10, activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
+
+    # generate dummy training data
+    x_train = np.random.random((1000, timesteps, data_dim))
+    y_train = np.random.random((1000, num_classes))
+
+    # generate dummy validation data
+    x_val = np.random.random((100, timesteps, data_dim))
+    y_val = np.random.random((100, num_classes))
+
+    model.fit(x_train, y_train,
+              batch_size=64, epochs=5,
+              validation_data=(x_val, y_val))
+
+
+def stateful_lstm_demo():
+    from keras.models import Sequential
+    from keras.layers import LSTM, Dense
+    import numpy as np
+
+    data_dim = 16
+    timesteps = 8
+    num_classes = 10
+    batch_size = 32
+
+    model = Sequential()
+    model.add(LSTM(32, return_sequences=True, stateful=True,
+                   batch_input_shape=(batch_size, timesteps, data_dim)))
+    model.add(LSTM(32, return_sequences=True, stateful=True))
+    model.add(LSTM(32, stateful=True))
+    model.add(Dense(10, activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
+
+    # Generate dummy training data
+    x_train = np.random.random((batch_size * 10, timesteps, data_dim))
+    y_train = np.random.random((batch_size * 10, num_classes))
+
+    # Generate dummy validation data
+    x_val = np.random.random((batch_size * 3, timesteps, data_dim))
+    y_val = np.random.random((batch_size * 3, num_classes))
+
+    model.fit(x_train, y_train,
+              batch_size=batch_size, epochs=5, shuffle=False,
+              validation_data=(x_val, y_val))
 
 
 if __name__ == '__main__':
@@ -128,5 +189,7 @@ if __name__ == '__main__':
         mlp_demo()
     elif args.model == 'vgg':
         vgg_demo()
-
-
+    elif args.model == 'lstm':
+        stack_lstm_demo()
+    elif args.model == 'lstm2':
+        stateful_lstm_demo()
