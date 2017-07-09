b7ac1f482e8baa83a22715086ed8fc81545662ea
awp4211 <1097028825@qq.com>
2017-07-09 08:46:09 +0800

    MNIST_TB

--- tensorboard/mnist_tensorboard.py
+++ tensorboard/mnist_tensorboard.py
@@ -10,53 +10,87 @@ mnist = input_data.read_data_sets(mnist_data_path, one_hot=True)
 print 'loading data done'
 
 
-def conv_layer(input, channels_in, channel_out):
-    w = tf.Variable(tf.zeros([5, 5, channels_in, channel_out]))
-    b = tf.Variable(tf.zeros([channel_out]))
-    conv = tf.nn.conv2d(input, w, strides=[1, 1, 1, 1], padding='SAME')
-    act = tf.nn.relu(conv + b)
-    return act
+LOG_DIR = os.path.join(os.getcwd(), 'tb_log_basic')
 
 
-def fc_layer(input, channels_in, channels_out):
-    w = tf.Variable(tf.zeros([channels_in, channels_out]))
-    b = tf.Variable(tf.zeros([channels_out]))
-    act = tf.nn.relu(tf.add(tf.matmul(input, w), b))
-    return act
+def conv_layer(input, channels_in, channel_out, name='conv'):
+    with tf.name_scope(name):
+        w = tf.Variable(tf.zeros([5, 5, channels_in, channel_out]))
+        b = tf.Variable(tf.zeros([channel_out]))
+        conv = tf.nn.conv2d(input, w, strides=[1, 1, 1, 1], padding='SAME')
+        act = tf.nn.relu(conv + b)
+        tf.summary.histogram("weight", w)
+        tf.summary.histogram("biases", b)
+        tf.summary.histogram("activations", act)
+        return act
 
 
-x = tf.placeholder(tf.float32, shape=[None, 784])
-y = tf.placeholder(tf.float32, shape=[None, 10])
-x_image = tf.reshape(x, [-1, 28, 28, 1])
+def fc_layer(input, channels_in, channels_out, name='fc'):
+    with tf.name_scope(name):
+        w = tf.Variable(tf.zeros([channels_in, channels_out]))
+        b = tf.Variable(tf.zeros([channels_out]))
+        act = tf.nn.relu(tf.add(tf.matmul(input, w), b))
+        tf.summary.histogram("weights", w)
+        tf.summary.histogram("biases", b)
+        tf.summary.histogram("activations", act)
+        return act
 
-conv1 = conv_layer(x_image, 1, 32)
-pool1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-conv2 = conv_layer(pool1, 32, 64)
-pool2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-flattened = tf.reshape(pool2, [-1, 7*7*64])
 
-fc1 = fc_layer(flattened, 7*7*64, 1024)
-logits = fc_layer(fc1, 1024, 10)
+def mnist_model():
 
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y))
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+    sess = tf.Session()
 
-# compute the accuracy
-correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(y, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    x = tf.placeholder(tf.float32, shape=[None, 784])
+    y = tf.placeholder(tf.float32, shape=[None, 10])
+    x_image = tf.reshape(x, [-1, 28, 28, 1])
+    tf.summary.image('input', x_image, 3)
 
-with tf.Session() as sess:
+    conv1 = conv_layer(x_image, 1, 32, name='conv1')
+    pool1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    conv2 = conv_layer(pool1, 32, 64, name='conv2')
+    pool2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    flattened = tf.reshape(pool2, [-1, 7*7*64])
+
+    fc1 = fc_layer(flattened, 7*7*64, 1024, name='fc1')
+    logits = fc_layer(fc1, 1024, 10, name='fc2')
+
+    with tf.name_scope('xent'):
+        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y))
+        tf.summary.scalar("xent", cross_entropy)
+
+    with tf.name_scope('train'):
+        train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+
+
+    with tf.name_scope('accuracy'):
+        # compute the accuracy
+        correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(y, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        tf.summary.scalar("accuracy", accuracy)
+
+    summ = tf.summary.merge_all()
+
     sess.run(tf.global_variables_initializer())
+    writer = tf.summary.FileWriter(LOG_DIR)
+    writer.add_graph(sess.graph)
 
     for i in tqdm(range(2000)):
-        batch_x, batch_y = mnist.train.next_batch(50)
+        batch_x, batch_y = mnist.train.next_batch(100)
+
+        if i % 5 == 0:
+            [s] = sess.run([summ],
+                            feed_dict={x: batch_x,
+                                           y: batch_y})
+            writer.add_summary(s, i)
 
         if (i+1) % 500 == 0:
-            print batch_x.shape
-            print batch_y.shape
-            train_accuracy = sess.run([accuracy], feed_dict={x: batch_x,
-                                                             y: batch_y})
+            [train_accuracy] = sess.run([accuracy], feed_dict={x: batch_x,
+                                                               y: batch_y})
             print 'step %d, training accuracy %g' % (i, train_accuracy)
 
         sess.run(train_step, feed_dict={x: batch_x,
                                         y: batch_y})
+
+
+if __name__ == '__main__':
+    mnist_model()
