397d941d2711fa410f673454af3c64aedc20c2d3
awp4211 <1097028825@qq.com>
2017-09-03 15:04:07 +0800

    utils

--- dcgan/utils.py
+++ dcgan/utils.py
@@ -23,8 +23,8 @@ def conv_out_size_same(size, stride):
 
 
 def show_all_variables():
-  model_vars = tf.trainable_variables()
-  slim.model_analyzer.analyze_vars(model_vars, print_info=True)
+    model_vars = tf.trainable_variables()
+    slim.model_analyzer.analyze_vars(model_vars, print_info=True)
 
 def get_image(image_path, input_height, input_width,
               resize_height=64, resize_width=64,
@@ -34,64 +34,72 @@ def get_image(image_path, input_height, input_width,
                    resize_height, resize_width, crop)
 
 def save_images(images, size, image_path):
-  return imsave(inverse_transform(images), size, image_path)
+    return imsave(inverse_transform(images), size, image_path)
+
 
 def imread(path, grayscale = False):
-  if (grayscale):
-    return scipy.misc.imread(path, flatten = True).astype(np.float)
-  else:
-    return scipy.misc.imread(path).astype(np.float)
+    if (grayscale):
+        return scipy.misc.imread(path, flatten = True).astype(np.float)
+    else:
+        return scipy.misc.imread(path).astype(np.float)
+
 
 def merge_images(images, size):
-  return inverse_transform(images)
+    return inverse_transform(images)
 
 def merge(images, size):
-  h, w = images.shape[1], images.shape[2]
-  if (images.shape[3] in (3,4)):
-    c = images.shape[3]
-    img = np.zeros((h * size[0], w * size[1], c))
-    for idx, image in enumerate(images):
-      i = idx % size[1]
-      j = idx // size[1]
-      img[j * h:j * h + h, i * w:i * w + w, :] = image
-    return img
-  elif images.shape[3]==1:
-    img = np.zeros((h * size[0], w * size[1]))
-    for idx, image in enumerate(images):
-      i = idx % size[1]
-      j = idx // size[1]
-      img[j * h:j * h + h, i * w:i * w + w] = image[:,:,0]
-    return img
-  else:
-    raise ValueError('in merge(images,size) images parameter '
+    """
+    merge images to one image
+    :param images: a set of image, shape=(batch, height, width, channels) 
+    :param size: 
+    :return: 
+    """
+    h, w = images.shape[1], images.shape[2]
+    if (images.shape[3] in (3,4)):
+        c = images.shape[3]
+        img = np.zeros((h * size[0], w * size[1], c))
+        for idx, image in enumerate(images):
+            i = idx % size[1]
+            j = idx // size[1]
+            img[j * h:j * h + h, i * w:i * w + w, :] = image
+        return img
+    elif images.shape[3]==1:
+        img = np.zeros((h * size[0], w * size[1]))
+        for idx, image in enumerate(images):
+            i = idx % size[1]
+            j = idx // size[1]
+            img[j * h:j * h + h, i * w:i * w + w] = image[:, :, 0]
+        return img
+    else:
+        raise ValueError('in merge(images,size) images parameter '
                      'must have dimensions: HxW or HxWx3 or HxWx4')
 
+
 def imsave(images, size, path):
-  image = np.squeeze(merge(images, size))
-  return scipy.misc.imsave(path, image)
+    image = np.squeeze(merge(images, size))
+    return scipy.misc.imsave(path, image)
 
 def center_crop(x, crop_h, crop_w,
                 resize_h=64, resize_w=64):
-  if crop_w is None:
-    crop_w = crop_h
-  h, w = x.shape[:2]
-  j = int(round((h - crop_h)/2.))
-  i = int(round((w - crop_w)/2.))
-  return scipy.misc.imresize(
-      x[j:j+crop_h, i:i+crop_w], [resize_h, resize_w])
+    if crop_w is None:
+        crop_w = crop_h
+        h, w = x.shape[:2]
+        j = int(round((h - crop_h)/2.))
+        i = int(round((w - crop_w)/2.))
+        return scipy.misc.imresize(x[j:j+crop_h, i:i+crop_w], [resize_h, resize_w])
 
 def transform(image, input_height, input_width,
               resize_height=64, resize_width=64, crop=True):
-  if crop:
-    cropped_image = center_crop(
-      image, input_height, input_width,
-      resize_height, resize_width)
-  else:
-    cropped_image = scipy.misc.imresize(image, [resize_height, resize_width])
-  return np.array(cropped_image)/127.5 - 1.
+    if crop:
+        cropped_image = center_crop(
+        image, input_height, input_width,
+          resize_height, resize_width)
+    else:
+        cropped_image = scipy.misc.imresize(image, [resize_height, resize_width])
+    return np.array(cropped_image)/127.5 - 1.
 
 def inverse_transform(images):
-  return (images+1.)/2.
+    return (images+1.)/2.
 
 def to_json(output_path, *layers):
   with open(output_path, "w") as layer_f:
@@ -248,7 +256,12 @@ def visualize(sess, dcgan, config, option):
 
 
 def image_manifold_size(num_images):
-  manifold_h = int(np.floor(np.sqrt(num_images)))
-  manifold_w = int(np.ceil(np.sqrt(num_images)))
-  assert manifold_h * manifold_w == num_images
-  return manifold_h, manifold_w
+    """
+    get new image rows and cols
+    :param num_images: 
+    :return: 
+    """
+    manifold_h = int(np.floor(np.sqrt(num_images)))
+    manifold_w = int(np.ceil(np.sqrt(num_images)))
+    assert manifold_h * manifold_w == num_images
+    return manifold_h, manifold_w
