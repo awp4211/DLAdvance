77bc5b42a1cd34108fc049d55ab8073988ecaaa6
awp4211 <1097028825@qq.com>
2017-09-04 23:02:55 +0800

    update

--- cifar_multigpu/cifar10_multi_gpu_train.py
+++ cifar_multigpu/cifar10_multi_gpu_train.py
@@ -51,7 +51,7 @@ import cifar10
 
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_string('train_dir', '/tmp/cifar10_train',
+tf.app.flags.DEFINE_string('train_dir', os.path.join(os.getcwd(), 'tb_log'),
                            """Directory where to write event logs """
                            """and checkpoint.""")
 tf.app.flags.DEFINE_integer('max_steps', 1000000,
