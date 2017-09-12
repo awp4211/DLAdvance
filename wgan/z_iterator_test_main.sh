6f20eb34ad35afce2a002368da5716b64f10e425
awp4211 <1097028825@qq.com>
2017-09-12 21:54:11 +0800

    run method

---
+++ wgan/z_iterator_test_main.sh
@@ -0,0 +1,3 @@
+python main.py --logs_dir=logs/CelebA_WGAN_logs4/ --optimizer=RMSProp --learning_rate=3e-5 --optimizer_param=0.9 --model=1 --iterations=1e5 --mode=train --data_dir=./ --mode=z_iterator_visualize --batch_size=64
+
+
---
+++ wgan/z_iterator_tsne_test_main.sh
@@ -0,0 +1,3 @@
+python main.py --logs_dir=logs/CelebA_WGAN_logs4/ --optimizer=RMSProp --learning_rate=1e-3 --optimizer_param=0.9 --model=1 --iterations=5e5 --mode=train --data_dir=./ --mode=z_iterator_tsne --batch_size=64 --nr_batches_tsne=1 --nr_iter_vis=1501
+
+
