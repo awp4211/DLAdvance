20d27fb7b935f3bee7ccd992277171bd33ad187f
awp4211 <1097028825@qq.com>
2017-09-20 08:30:30 +0800

    run

--- patch_gan/main.py
+++ patch_gan/main.py
@@ -1,10 +1,10 @@
 import os
 import numpy as np
-import scipy.misc
 import tensorflow as tf
 
-from model import PatchGAN
-from utils import pp, visualize, to_json, show_all_variables
+#from model_v1 import PatchGAN
+from model_v4 import PatchGAN
+from utils import pp, visualize, show_all_variables
 
 
 flags = tf.app.flags
@@ -13,8 +13,8 @@ flags.DEFINE_float("learning_rate", 0.0002, "Learning rate of for adam [0.0002]"
 flags.DEFINE_float("beta1", 0.5, "Momentum term of adam [0.5]")
 flags.DEFINE_boolean("train", True, "True for training, False for testing [False]")
 
-flags.DEFINE_integer("epoch", 10000, "Epoch to train [25]")
-flags.DEFINE_integer("g_epoch", 2, "Iteration of training G when training D one time")
+flags.DEFINE_integer("epoch", 1000, "Epoch to train [25]")
+flags.DEFINE_integer("g_epoch", 10, "Iteration of training G when training D one time")
 flags.DEFINE_integer("train_size", np.inf, "The size of train images [np.inf]")
 flags.DEFINE_integer("batch_size", 64, "The size of batch images [64]")
 
@@ -30,7 +30,7 @@ flags.DEFINE_string("sample_dir", "samples", "Directory name to save the image s
 #flags.DEFINE_boolean("crop", False, "True for training, False for testing [False]")
 flags.DEFINE_boolean("visualize", False, "True for visualizing, False for nothing [False]")
 
-flags.DEFINE_integer("z_dim", 25, "noise dimension")
+flags.DEFINE_integer("z_dim", 100, "noise dimension")
 FLAGS = flags.FLAGS
 
 
