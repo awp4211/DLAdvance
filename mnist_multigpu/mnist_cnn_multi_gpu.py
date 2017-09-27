07051226e1c1982e7d0dc7e13d5acb8baa7f56a5
awp4211 <1097028825@qq.com>
2017-09-27 09:45:42 +0800

    supervised multi-gpu framework

--- mnist_multigpu/mnist_cnn_multi_gpu.py
+++ mnist_multigpu/mnist_cnn_multi_gpu.py
@@ -55,14 +55,134 @@ def build_model(x):
     return y
 
 
+def average_gradients(tower_grads):
+    average_grads = []
+    for grad_and_vars in zip(*tower_grads):
+        # note that each grad_and_vars looks like the following:
+        # ((grad_gpu_0, var0_gpu0), ..., (grad_gpu_N, var0_gpuN) )
+        grads = [g for g, _ in grad_and_vars]
+        # average over the "tower" dimension
+        grad = tf.stack(grads, 0)
+        grad = tf.reduce_mean(grad, 0)
 
-def average_losses(loss):
-    tf.add_to_collection("losses", loss)
-
-    # assemble all of the losses for the current tower only
-    losses = tf.get_collection("losses")
-
-    # calculate the total loss for the current tower
-    regularization
+        # keep in mind that the variables are redundant because they are shared
+        # accross towers. So we will just return the first tower's pointer to the variables
+        v = grad_and_vars[0][1]
+        gran_and_var = (grad, v)
+    return average_grads
 
 
+def feed_all_gpu(input_dict, models, payload_per_gpu, batch_x, batch_y):
+    for i in range(len(models)):
+        x, y, _, _, _ = models[i]
+        start_pos = i * payload_per_gpu
+        stop_pos = (i+1) * payload_per_gpu
+        input_dict[x] = batch_x[start_pos:stop_pos]
+        input_dict[y] = batch_y[start_pos:stop_pos]
+    return input_dict
+
+
+def multi_gpu(num_gpu):
+    batch_size = 128*num_gpu
+    mnist_data_path = os.path.join(os.path.dirname(os.getcwd()), 'MNIST_DATA')
+    mnist = input_data.read_data_sets(mnist_data_path, one_hot=True)
+
+    tf.reset_default_graph()
+    with tf.Session() as sess:
+        with tf.device("/cpu:0"):
+            learning_rate = tf.placeholder(tf.float32, shape=[])
+            opt = tf.train.AdamOptimizer(learning_rate=learning_rate)
+
+            print "building model on gpu tower ..."
+            models = []
+            for gpu_id in range(num_gpu):
+                with tf.device("/gpu:%d" % gpu_id):
+                    print "tower:%d ..." % gpu_id
+                    with tf.name_scope("tower_%d" % gpu_id):
+                        with tf.variable_scope("cpu_variables", reuse=gpu_id>0):
+                            x = tf.placeholder(tf.float32, [None, 784])
+                            y = tf.placeholder(tf.float32, [None, 10])
+                            pred = build_model(x)
+                            loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
+                            grads = opt.compute_gradients(loss)
+                            models.append((x, y, pred, loss, grads))
+            print "build model on gpu tower done"
+
+            print "reduce model on cpu ..."
+            tower_x, tower_y, tower_preds, tower_losses, tower_grads = zip(*models)
+            aver_loss_op = tf.reduce_mean(tower_losses)
+            apply_gradient_op = opt.apply_gradients(average_gradients(tower_grads))
+
+            all_y = tf.reshape(tf.stack(tower_y, 0), [-1, 10])
+            all_pred = tf.reshape(tf.stack(tower_preds, 0), [-1, 10])
+            correct_pred = tf.equal(tf.argmax(all_y, 1), tf.argmax(all_pred, 1))
+            accuracy = tf.reduce_mean(tf.cast(correct_pred, "float"))
+            print "reduce model on cpu done ..."
+
+            print "run train op ..."
+            sess.run(tf.global_variables_initializer())
+            lr = 0.01
+            for epoch in range(2):
+                start_time = time.time()
+                payload_per_gpu = batch_size / num_gpu
+                total_batch = int(mnist.train.num_examples/batch_size)
+                avg_loss = 0.0
+                print "\n-----------------------"
+                print "epoch:%d, lr:%.4f" % (epoch, lr)
+
+                for batch_idx in range(total_batch):
+                    batch_x, batch_y = mnist.train.next_batch(batch_size)
+                    input_dict = {}
+                    input_dict[learning_rate] = lr
+                    input_dict = feed_all_gpu(input_dict, models, payload_per_gpu, batch_x, batch_y)
+                    _, _loss = sess.run([apply_gradient_op, aver_loss_op], input_dict)
+                    avg_loss += _loss
+
+                avg_loss /= total_batch
+                print "training loss:%.4f" % avg_loss
+                lr = max(lr*0.7, 0.00001)
+
+                # validation
+                val_payload_per_gpu = batch_size / num_gpu
+                total_batch = int(mnist.validation.num_examples / batch_size)
+                preds = None
+                ys = None
+                for batch_idx in range(total_batch):
+                    batch_x, batch_y = mnist.validation.next_batch(batch_size)
+                    input_dict = feed_all_gpu({}, models, val_payload_per_gpu, batch_x, batch_y)
+                    batch_pred, batch_y = sess.run([all_pred, all_y], input_dict)
+
+                    preds = batch_pred if preds is None else np.concatenate((preds, batch_pred),0)
+                    ys = batch_y if ys is None else np.concatenate((ys, batch_y), 0)
+                val_accuracy = sess.run([accuracy], {all_y: ys, all_pred:preds})[0]
+                print "val accuracy: %0.4f%%" % (100.0 * val_accuracy)
+
+                stop_time = time.time()
+                elapsed_time = stop_time - start_time
+                print "cost time: " + str(elapsed_time) + " sec."
+            print "training done."
+
+            # test model
+            test_payload_per_gpu = batch_size / num_gpu
+            total_batch = int(mnist.test.num_examples / batch_size)
+            preds = None
+            ys = None
+            for batch_idx in range(total_batch):
+                batch_x, batch_y = mnist.test.next_batch(batch_size)
+                inp_dict = feed_all_gpu({}, models, test_payload_per_gpu, batch_x, batch_y)
+                batch_pred, batch_y = sess.run([all_pred, all_y], inp_dict)
+                if preds is None:
+                    preds = batch_pred
+                else:
+                    preds = np.concatenate((preds, batch_pred), 0)
+                if ys is None:
+                    ys = batch_y
+                else:
+                    ys = np.concatenate((ys, batch_y), 0)
+            test_accuracy = sess.run([accuracy], {all_y: ys, all_pred: preds})[0]
+            print('Test Accuracy: %0.4f%%\n\n' % (100.0 * test_accuracy))
+
+
+if __name__ == "__main__":
+    for num_gpu in range(4):
+        multi_gpu(num_gpu)
